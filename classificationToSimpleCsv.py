20e696dedc922cb7b913bbc0a1ce7c6f68e5a6b5
StorkST <vchd@pm.me>
2020-05-01 10:48:42 -0400

    rename script, add more stats details

--- classificationToSimpleCsv.py
+++ classificationToSimpleCsv.py
@@ -1,7 +1,11 @@
 import argparse
 import csv
 
-levels = ["A1", "A2", "B1"]
+#levels = ["A1", "A2", "B1"]
+levels = ["B2"]
+#levels = ["C1","C2"]
+lengthVerb = 23
+lengthTrans = 17
 
 with open("RussianVerbsClassification.csv", 'r', newline='') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
@@ -23,30 +27,34 @@ with open("RussianVerbsClassification.csv", 'r', newline='') as csvfile:
             if lvl in levels:
                 if verbRank != "10000":
                     #print(pair)
-                    if len(pair) > 22:
-                        bigPairs += 1
-                        bigPairsA.append(pair)
+                    if len(pair) > lengthVerb:
+                        pairA = pair.split('/')
+                        imperf = pairA[0]
+                        perf = pairA[1]
+                        if not (perf.endswith(imperf) and (len(perf) + 2) <= lengthVerb):
+                            bigPairs += 1
+                            bigPairsA.append(pair)
                     #print(transFR)
                     #print(transEN)
-                    if len(transFR) > 18:
+                    if len(transFR) > lengthTrans:
                         bigTransFR += 1
                         bigTransFRA.append(transFR)
-                    if len(transEN) > 18:
+                    if len(transEN) > lengthTrans:
                         bigTransEN += 1
                         bigTransENA.append(transEN)
 
     print("num row: " + str(i))
     print("------------------------------------------------")
-    print("num pairs > 22 chars: " + str(bigPairs))
+    print("num pairs > " + str(lengthVerb) + " chars: " + str(bigPairs))
     print("big pairs: " + str(bigPairsA))
     print("longest pairs: ")
     ordBigPairsA = sorted(bigPairsA, key=len)
     for pair in ordBigPairsA:
-        print(pair + " :" + str(len(pair)))
+        print(pair + ";" + str(len(pair)))
 
     print("------------------------------------------------")
-    print("num FR translation > 18 chars: " + str(bigTransFR))
+    print("num FR translation > " + str(lengthTrans) + " chars: " + str(bigTransFR))
     print("big FR translation: " + str(bigTransFRA))
     print("------------------------------------------------")
-    print("num EN translation > 18 chars: " + str(bigTransEN))
+    print("num EN translation > " + str(lengthTrans) + " chars: " + str(bigTransEN))
     print("big EN translation: " + str(bigTransENA))
